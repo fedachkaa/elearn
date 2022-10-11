ceeac86e43838ec4e5d69770e480cf212756b557
Aleksandra Fedak <fedak.aleksandra@student.uzhnu.edu.ua>
2022-10-11 22:27:20 +0300

    Create user registration and authorization

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,18 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/elearn', function () {
     return view('welcome');
+})->name('home');
+
+Route::group(['middleware'=>'guest'], function(){
+    Route::get('elearn/register', [UserController::class, 'create'])->name('register.create');
+    Route::post('elearn/register', [UserController::class, 'store'])->name('register.store');
+    Route::get('elearn/login', [UserController::class, 'loginForm'])->name('login.create');
+    Route::post('elearn/login', [UserController::class, 'login'])->name('login');
 });
+
+Route::get('elearn/logout', [UserController::class, 'logout'])->name('logout')->middleware('auth');
+
